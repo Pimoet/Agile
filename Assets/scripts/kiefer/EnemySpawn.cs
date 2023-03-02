a7dde580480dfa8159c95cabce04cb04b7fc2b02
kieferperquin <91886036+kieferperquin@users.noreply.github.com>
2023-03-02 16:33:26 +0100

    Update EnemySpawn.cs

--- Assets/scripts/kiefer/EnemySpawn.cs
+++ Assets/scripts/kiefer/EnemySpawn.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
+    #region variables
+    public GameObject normalZombie;
+    public GameObject fastZombie2;
+    public GameObject tankZombie;
+
+    int playerPos;
+    int spawnRadius = 50;
+
+    int maxYSpawn;
+    int minYSpawn;
+    int maxXSpawn;
+    int minXSpawn;
+    #endregion
+
     void Start()
     {
-        
+        playerPos = playerPos -1;
+        maxYSpawn = playerPos + spawnRadius;
+        minYSpawn = playerPos - spawnRadius;
+        maxXSpawn = playerPos + spawnRadius;
+        minXSpawn = playerPos - spawnRadius;
+        int name = Random.Range(1, 51);
     }
 
     void Update()
     {
-        
     }
 }
