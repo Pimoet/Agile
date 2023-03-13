a20dc0a18f56aba5d525d5fc6dcec99ce8dcffa9
kieferperquin <91886036+kieferperquin@users.noreply.github.com>
2023-03-13 12:12:51 +0100

    Update EnemySpawn.cs

--- Assets/scripts/kiefer/EnemySpawn.cs
+++ Assets/scripts/kiefer/EnemySpawn.cs
@@ -9,26 +9,33 @@ public class EnemySpawn : MonoBehaviour
     public GameObject fastZombie2;
     public GameObject tankZombie;
 
-    int playerPos;
-    int spawnRadius = 50;
-
-    int maxYSpawn;
-    int minYSpawn;
-    int maxXSpawn;
-    int minXSpawn;
+    public int amount;
+    private Vector3 spawnPoint;
+    private float camMin;
+    private float camMax;
     #endregion
+
 
     void Start()
     {
-        playerPos = playerPos -1;
-        maxYSpawn = playerPos + spawnRadius;
-        minYSpawn = playerPos - spawnRadius;
-        maxXSpawn = playerPos + spawnRadius;
-        minXSpawn = playerPos - spawnRadius;
-        int name = Random.Range(1, 51);
+        camMin = Camera.main.orthograp$$anonymous$$cSize;
+        camMax = Camera.main.orthograp$$anonymous$$cSize + 2;
     }
-
     void Update()
     {
+        enemy = GameObject.FindGameObjectsWithTag("Enemy");
+        amount = enemy.Length;
+        if (amount != 10)
+        {
+            InvokeRepeating("spawnEnemy", 1f, 2f);
+        }
+    }
+    void spawnEnemy()
+    {
+        spawnPoint.x = Random.Range(camMin, camMax);
+        spawnPoint.y = Random.Range(camMin, camMax);
+        spawnPoint.z = 0;
+        Instantiate(enemy[UnityEngine.Random.Range(0, enemy.Length - 1)], spawnPoint, Quaternion.identity);
+        CancelInvoke();
     }
 }
