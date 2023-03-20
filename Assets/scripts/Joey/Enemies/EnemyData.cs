4000933514d2503fafd540562e6442c6314aaea4
AliceEverglade <91664294+AliceEverglade@users.noreply.github.com>
2023-03-20 10:40:40 +0100

    Enemy Data, Player Data and Player Animations

--- Assets/scripts/Joey/Enemies/EnemyData.cs
+++ Assets/scripts/Joey/Enemies/EnemyData.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName ="Zombie")]
 public class EnemyData : ScriptableObject
 {
     public string EnemyName;
+    public GameObject EnemyPrefab;
 
     public float maxHealth;
     public float movementSpeed;
